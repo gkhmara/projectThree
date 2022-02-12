e9310c359c91199fdfa031f121966cf49c311ca8
Greg Khmara <gkhmara@gmail.com>
2022-02-11 21:59:07 -0800

    Work to Improve function and loop to isolate numbers 1 or 2 or 3.

--- js/scripts.js
+++ js/scripts.js
@@ -86,20 +86,63 @@ testArray;
 
 //1-4
 //const array = [13, 12, 23];
-const array = [5, 5, "13", 9]
+const array = ["13", 5, 7, "2"]
 
-let testVariable;
+let tempHold;
+
+let testVariable = [];
 
 let testArray = [];
 
 function splitNumbers() {
   for (let i = 0; i < array.length; i++) {
    
-    if (typeof(array[i]) != 'number') {
-      alert(array[i]);
-      array[i] = array.join("");
+    if (typeof(array[i]) === 'number') {
+      testVariable.push(array[i]);
+      tempHold = testVariable.join('');
+      tempHold = parseInt(tempHold);
+    }
+  }
+}
+
+function replace() {
+  for (let i = 0; i < array.length; i++) {
+   
+    if (typeof(array[i]) === 'number') {
+      array[i] = tempHold;
+    }
+  }
+}
+
+// function deleteDuplicate() {
+//     const id = array.indexOf(57);
+//     array.splice(id, 1);
+//   }
+
+function deleteDuplicate() {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array [i + 1]) {
+      const id = array.indexOf(i);
+      array.splice(id, i);
+      alert(array.indexOf(i));
     }
   }
 }
 
 splitNumbers();
+replace();
+deleteDuplicate();
+console.log(array);
+
+
+//1-5
+//const array = [13, 12, 23];
+const array = ["13", 5, 7, "2"]
+
+let originalString = 'GeeksForGeeks';
+let newString;
+
+function removeCharacter() {
+  newString = originalString.replace('F', '');
+
+}
