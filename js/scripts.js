babce21b8134826f200f7e15be5cd7e37877e61e
Greg Khmara <gkhmara@gmail.com>
2022-02-12 16:25:41 -0800

    Make improvements to JavaScript and jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,73 @@
-const numbers = ["13", "12", "23"];
+
+let one = "Beep";
+let userArray = [];
+
+function myFunction() {
+  num = document.getElementById("num").value;
+    if (num) {
+
+      for(let i = 0; i <= num; i++){
+        userArray.push(i);
+      }
+
+    alert(userArray);
+
+    }
+
+    if (userArray[3] === 3) {
+      userArray[3] === one;
+    }
+
+
+
+}
+
+
+// userArray.forEach(function(element, index) {
+//   if (element.includes("3")) {
+//     userArray[index] = "Won't you be my neighbor?";
+//   } else if (element.includes("2")) {
+//     userArray[index] = "Boop!";
+//   } else if (element.includes("1")) {
+//     userArray[index] = "Beep!";
+//   }
+// });
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $("#userInput").submit(function(event){
+    
+    let numberInput = userArray;
+    alert("num = " + num);
+    
+
+    //Name Question
+    if (Boolean(numberInput) === false) {
+      $("#numberOutput").hide();
+      $("#error").show();
+    } else if (Boolean(numberInput) === true){
+      $(".number").text(numberInput);
+      $("#error").hide();
+      $("#numberOutput").show();
+    }
+
+    //Show Final Results
+    $("#results").show();
+
+
+    event.preventDefault();
+  });
+});
+
+
+
+const numbers = ["3","2", "1", "12", "32", "99"];
 
 numbers.forEach(function(element, index) {
     if (element.includes("3")) {
